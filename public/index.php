0c5ee3ce24d61be5d34e43502b7c5bbcf5e4ba9b
mariano escalera <quarkmarino@gmail.com>
2015-03-12 11:16:07 -0600

    Adds blade template, and basic views and layout

--- public/index.php
+++ public/index.php
@@ -1,10 +1,19 @@
 <?php
 	require('../vendor/autoload.php');
 
-	$app = new \Slim\Slim();
+	$app = new \Slim\Slim([
+		'view' => new \Slim\Views\Blade(),
+		'templates.path' => '../templates'
+	]);
 
-	$app->get('/hey', function(){
-		echo "hi there!!";
+	$views = $app->view();
+	$views->parserOptions = [
+		'debug' => true,
+		'cache' => '../html_cache'
+	];
+
+	$app->get('/hey', function() use ($app){
+		$app->render('index');
 	});
 
 	$app->run();
