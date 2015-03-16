63f3a80ec09c76d1f0f6a6d97382561a0610551c
quarkmarino <quarkmarino@gmail.com>
2015-03-16 16:09:37 -0600

    uses oscarotero/form manager to design the complex former _form view that handles the links' creation and edition

--- templates/links/_form.blade.php
+++ templates/links/_form.blade.php
@@ -1,20 +1,19 @@
-@if(isset($link))
-	<form action="{{$app->urlFor('link.update', ['id' => $link->id])}}" method="POST" role="form">
-		<legend>Edit link</legend>
-@else
-	<form action="{{$app->urlFor('link.store')}}" method="POST" role="form">
-		<legend>Add new link</legend>
-@endif
-		
-
-		<div class="form-group">
-			<label for="">Link Name</label>
-			<input type="text" class="form-control" id="link_name" name="name" placeholder="Link Name" value="@if(isset($link)){{$link->name}}@endif">
-			<label for="">Link url</label>
-			<input type="text" class="form-control" id="link_url" name="url" placeholder="Link Url" value="@if(isset($link)){{$link->url}}@endif">
-			<label for="">Link status</label>
-			<input type="text" class="form-control" id="link_status" name="status" placeholder="Link Status" value="@if(isset($link)){{$link->status}}@endif">
-		</div>
-
-		<button type="submit" class="btn btn-primary pull-right">Submit Link</button>
-	</form>
+{{$form = FormManager\Builder::form()}}
+{{
+	$form->attr([
+	    'action' => isset($link) ? $app->urlFor('link.update', ['id' => $link->id]) : $app->urlFor('link.store'),
+	    'method' => 'post'
+	])->add([
+	    'name' => FormManager\Builder::text()->addClass('form-control')->id('link_name')->placeholder('Name ej. twitter')->maxlength(64)->required()->label('Link Name'),
+	    'url' => FormManager\Builder::text()->addClass('form-control')->id('link_url')->placeholder('Url ej. http://twitter.com')->maxlength(64)->required()->label('Link Url'),
+	    'status' => FormManager\Builder::select()->options([
+	    	'' => '-- Choose a Status --',
+			'favorite' => 'Favoritos',
+			'important' => 'Importante',
+			'great' => 'Genial'
+	    ])->addClass('form-control')->id('link_status')->required()->label('Link Status'),
+	    'action' => FormManager\Builder::choose([
+	        'save' => FormManager\Builder::submit()->addClass('btn btn-primary pull-right')->html('Save Link')
+	    ])
+	])
+}}
