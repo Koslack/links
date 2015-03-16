9708bb6593882f2f3e2d855dde8017ae6a5fb5ce
quarkmarino <quarkmarino@gmail.com>
2015-03-16 15:32:15 -0600

    updates DB schema links table to use 'name' column

--- store/app_schema.sql
+++ store/app_schema.sql
@@ -1,13 +1,14 @@
 CREATE TABLE links (
 	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	name CHAR(128) NOT NULL,
 	url CHAR(128) NOT NULL,
 	status CHAR(128) NOT NULL DEFAULT 'favorite'
 );
 
 INSERT INTO links
-	(id, url, status)
+	(id, name, url, status)
 VALUES
-	(NULL, 'http://google.com/', 'important'),
-	(NULL, 'http://facebook.com/', 'favorites'),
-	(NULL, 'http://gmail.com/', 'favorites'),
-	(NULL, 'http://youtube.com/', 'favorites');
+	(NULL, 'google', 'http://google.com/', 'important'),
+	(NULL, 'facebook', 'http://facebook.com/', 'favorites'),
+	(NULL, 'gmail', 'http://gmail.com/', 'favorites'),
+	(NULL, 'youtube', 'http://youtube.com/', 'favorites');
